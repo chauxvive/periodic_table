6b32184fc1faebe6ac6457cddb9a022cf6202810
Mary Paul <mpauldesigns@gmail.com>
2025-04-12 14:01:39 -0400

    final commit

--- element.sh
+++ element.sh
@@ -1,3 +1,33 @@
 #!/bin/bash
 
-echo "Please provide an element as an argument."
+PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
+
+# Check for input
+if [[ -z $1 ]]
+then
+  echo "Please provide an element as an argument."
+  exit
+fi
+
+# Get element info from joined tables
+ELEMENT_INFO=$($PSQL "
+SELECT e.atomic_number, e.name, e.symbol, t.type, p.atomic_mass, p.melting_point_celsius, p.boiling_point_celsius
+FROM elements e
+JOIN properties p ON e.atomic_number = p.atomic_number
+JOIN types t ON p.type_id = t.type_id
+WHERE e.atomic_number::text = '$1' OR e.symbol = '$1' OR e.name = '$1';
+")
+
+# Handle no match
+if [[ -z $ELEMENT_INFO ]]
+then
+  echo "I could not find that element in the database."
+  exit
+fi
+
+# Parse result into variables
+IFS="|" read ATOMIC_NUMBER NAME SYMBOL TYPE MASS MELT BOIL <<< "$ELEMENT_INFO"
+
+# Final output
+echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MELT celsius and a boiling point of $BOIL celsius."
+
